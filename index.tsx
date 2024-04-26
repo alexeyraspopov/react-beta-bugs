2106456cd7c53f01d756063242c4fe36c9dc706f
Oleksii Raspopov <oleksii.raspopov@gmail.com>
2024-04-26 08:11:28 -0400

    version 19.0.0-beta-94eed63c49-20240425: use/useTransition bugs

---
+++ index.tsx
@@ -0,0 +1,111 @@
+import { use, useSyncExternalStore, useTransition, Suspense, useState, useMemo } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.querySelector("main")!).render(<App />);
+
+function App() {
+  return (
+    <section>
+      <p>
+        A simple controlled element that unwraps async value via use() and updates state via
+        useTransition().
+      </p>
+      <p>
+        Expected behavior: "Loading" during initial render; when clicking on the button it becomes
+        half transparent and then goes back to black with a new value without extra "Loading" state.
+      </p>
+      <SimpleAsyncFlow />
+      <hr />
+      <p>
+        Slightly more complicated async flow. The same use of use(), useTransition(), and Suspense,
+        but the target promise is delivered via synchronous event emitter that the component
+        subscribes to via useSyncExternalStore().
+      </p>
+      <p>
+        Expected behavior (same as for the case above): "Loading" during initial render; when
+        clicking on the button it becomes half transparent and then goes back to black with a new
+        value without extra "Loading" state.
+      </p>
+      <StoreBasedFlow />
+    </section>
+  );
+}
+
+function SimpleAsyncFlow() {
+  let [value, setValue] = useState(() => delayed(Math.random()));
+  return (
+    <Suspense fallback={<p>Loading</p>}>
+      <SimpleControlledDisplay promise={value} onChange={setValue} />
+    </Suspense>
+  );
+}
+
+function SimpleControlledDisplay({
+  promise,
+  onChange,
+}: {
+  promise: Promise<number>;
+  onChange: (value: Promise<number>) => void;
+}) {
+  let value = use(promise);
+  let [pending, startTransition] = useTransition();
+  let click = () => {
+    startTransition(() => {
+      onChange(delayed(Math.random()));
+    });
+  };
+  return (
+    <div>
+      <button onClick={click}>Refresh</button>
+      <p style={{ opacity: pending ? 0.5 : 1 }}>{value}</p>
+    </div>
+  );
+}
+
+function StoreBasedFlow() {
+  let atom = useMemo(() => createAtom(delayed(Math.random())), []);
+  return (
+    <Suspense fallback={<p>Loading</p>}>
+      <ExternallyControlledDisplay atom={atom} />
+    </Suspense>
+  );
+}
+
+function ExternallyControlledDisplay({ atom }: { atom: Atom<Promise<number>> }) {
+  let promise = useSyncExternalStore(atom.subscribe, atom.get);
+  let value = use(promise);
+  let [pending, startTransition] = useTransition();
+  let click = () => {
+    startTransition(() => {
+      atom.set(delayed(Math.random()));
+    });
+  };
+  return (
+    <div>
+      <button onClick={click}>Refresh</button>
+      <p style={{ opacity: pending ? 0.5 : 1 }}>{value}</p>
+    </div>
+  );
+}
+
+function delayed<T>(value: T) {
+  return new Promise<T>((resolve) => setTimeout(resolve, 1000, value));
+}
+
+type Atom<T> = ReturnType<typeof createAtom<T>>;
+
+function createAtom<T>(value: T, events = new EventTarget()) {
+  return {
+    get() {
+      return value;
+    },
+    set(newValue: T) {
+      value = newValue;
+      events.dispatchEvent(new Event("change"));
+    },
+    subscribe(cb) {
+      events.addEventListener("change", cb);
+      return () => events.removeEventListener("change", cb);
+    },
+  };
+}
